e6618c6aa2846a72be506ef20c9e186a6a079507
Nico TSESSILEDE <31391553+nicotsessilede@users.noreply.github.com>
2022-05-14 06:29:26 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,8 +26,8 @@ resource "azurerm_subnet" "backend" {
 
 resource "azurerm_public_ip" "myAGPublicIPAddress" {
   name                = "myAGPublicIPAddress"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.myResourceGroup.name
+  location            = azurerm_resource_group.myResourceGroup.location
   allocation_method   = "Dynamic"
 }
 
